f886deadefb277c4a081830eb92ddd0a4a21ef4d
agilzulfa16 <agilzul16@outlook.com>
2023-09-15 15:41:19 +0700

    Belajar php

---
+++ biodata.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2> <?php echo "Biodata"?> </h2>
+    
+
+    <TABle>
+        <tr>
+        <th> Nama</th>
+        <th> Tempat tanggal lahir</th>
+        <th> Hobi</th>
+        <th> Asal</th>
+        </tr>
+
+        <tr>
+            <td>Agil Zulfa</td>
+            <td>Bogor, 16 Oktober 2004</td>
+            <td>Pungsal</td>
+            <td>Bogor</td>
+        </tr>
+    </TABle>
+</body>
+</html>
+
+<style> 
+    body{
+        text-align: center;
+    }
+
+    table, th, td, tr {
+        justify-content: center;
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+</style>
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+    /* Ini Komentar */
+    // print "Rian<br>";
+    // // var_dump("Rian");
+
+    // echo "Pertemuan 3 - Pemrograman web<br>";
+    // echo "Hallo Bitches<br>";
+    // echo 911
+
+    // $nama = "Universitas Siliasih";
+    // echo $nama;
+
+    //Operator aritmatika 
+    // +-%/
+
+    // $x = 90;
+    // $y = 9;
+    // echo $x + $y;
+    // echo $x - $y;
+    // echo $x % $y;
+    // echo $x / $y;
+    // echo $x * $y;
+    
+    //penyambung variabel (.)
+    // $nama_depan = " Akbar";
+    // $nama_belakang = "Udin";
+
+    // echo $nama_depan . " " . $nama_belakang;
+
+    $a = 1;
+    $a += 2;
+    echo $a;
+    
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo "Pemrograman Web" ?></h1>
+    <?php echo "<h2> Hallo Bitches </h2>" ?>
+</body>
+</html>
